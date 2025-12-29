8d1f696ad53b51593d74556a5d0f64cc9820f956
Abdul Sami Wadho <samiwadho1988@gmail.com>
2025-12-29 13:47:34 +0100

    update

---
+++ Classes/1 Exam LAB.R
@@ -0,0 +1,27 @@
+4+4
+
+
+# this is commit 
+2*4^2
+
+#we can see that from very large or very small numbers R uses Scientific notation
+format(2*10^6)
+
+
+
+format(2*10^6, scientific = FALSE)
+
+
+12/0 # we can get infinite as a result
+
+log(0) # we can also get infinite as a result 
+
+sqrt(-3) # Nan Stands for "not a number"
+
+sqrt(3)
+
+
+?round # "?" this is for finding documentation of any model,function
+
+round(110.123456, 3)
+
