3af5b5fb1e3f4723e066d968de96a58a60e27b83
Abdul Sami Wadho <samiwadho1988@gmail.com>
2025-12-29 13:56:35 +0100

    update

--- Classes/1 Exam LAB.R
+++ Classes/1 Exam LAB.R
@@ -12,7 +12,7 @@ format(2*10^6)
 format(2*10^6, scientific = FALSE)
 
 
-12/0 # we can get infinite as a result
+12/0           # we can get infinite as a result
 
 log(0) # we can also get infinite as a result 
 
@@ -24,4 +24,42 @@ sqrt(3)
 ?round # "?" this is for finding documentation of any model,function
 
 round(110.123456, 3)
+
+# this is equivalent to
+round(x=10.123456,digit= 3)
+
+
+"statistics"  # this is a string of characters
+
+# below is single comma and still same result as a string with two comma
+
+'statistics'
+
+log(1)        # log is natural logarithm
+
+exp(1)          # exponential function
+
+pi
+
+
+                # create a variable and assign value to it 
+x<-12
+x
+
+
+y<-8 
+y
+
+#sum of two variable values
+z=x+y
+z
+
+
+
+                            # remove the object 
+rm(x) # remove the object 
+try(x) # finding the object 
+# result Error in eval(ei, envir) : object 'x' not found
+
+
 
