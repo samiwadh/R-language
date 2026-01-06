4bf741152ab56ce89042435964f1dcd8c809dcd5
Abdul Sami Wadho <samiwadho1988@gmail.com>
2026-01-06 19:47:07 +0100

    update

--- Classes/3 Exam LAB.R
+++ Classes/3 Exam LAB.R
@@ -1,1 +1,18 @@
-S
+library(tidyverse)
+library(gapminder)
+count(gapminder)
+# or
+gapminder|> summarize(n=n())
+
+dim(gapminder)
+summary(gapminder)
+
+nrow(gapminder)
+count(gapminder,continent)
+# OR
+count(x=gapminder, continents=continent)
+# Or 
+gapminder|>group_by(continent)|> summarize(n=n)
+
+tot<-summary(gapminder$continent)
+tot
